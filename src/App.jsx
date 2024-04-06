7f9266e95a6840fbf8cca3123ebc729e3b879cff
Raz Efron <razefron01@gmail.com>
2024-04-06 09:43:25 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Form.css";
+
+function Form() {
+  return (
+    <>
+      <form>
+        <input id="values" name="values" type="text" />
+        <select id="operation" name="operation">
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button type="submit">Calculate</button>
+      </form>
+      <section id="result">
+        <p></p>
+      </section>
+    </>
+  );
+}
+
+export default Form;
